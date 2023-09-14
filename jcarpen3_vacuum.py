318187f993764bf00ab874196272b3f9ed597f69
jcarpenter23 <93732603+jcarpenter23@users.noreply.github.com>
2023-09-14 19:36:45 -0400

    Update jcarpen3_vacuum.py
    complete change:
    
    percepts edit and simplify other methods

--- jcarpen3_vacuum.py
+++ jcarpen3_vacuum.py
@@ -10,38 +10,24 @@ class Jcarpen3VacuumAgent(VacuumAgent):
 
 
     def program(self, percept):
-        bump, status = percept
+        dirt, status = percept
+        
         possible_directions = ['Up', 'Down', 'Right', 'Left']
         
-        
-        if status == 'Dirty':
-            self.squares_cleaned.add(self.location)
+        if dirt == "Dirty":
             return 'Suck'
         
-        if bump:
-            possible_directions = ['Up', 'Down', 'Right', 'Left']
+        elif status == 'Bump':
             possible_directions.remove(self.direction)
             self.direction = random.choice(possible_directions)
-            return self.direction
-
         
-        next_square = self.get_next_square()
-        if next_square in self.squares_cleaned:
-            return self.direction
-        else:
-            return 'Suck'
+        
+        elif status == 'Clean':
+            self.direction = random.choice(possible_directions)
             
         return self.direction
-
-    def get_next_square(self):
-        x, y = self.location
-        if self.direction == 'Up':
-            return (x, y + 1)
-        elif self.direction == 'Down':
-            return (x, y - 1)
-        elif self.direction == 'Right':
-            return (x + 1, y)
-        elif self.direction == 'Left':
-            return (x - 1, y)
-
             
+            
+            
+    
+       
