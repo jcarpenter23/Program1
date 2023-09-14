a04db1b964d5e871115a7d19b16835643a824c7c
jcarpenter23 <93732603+jcarpenter23@users.noreply.github.com>
2023-09-14 17:35:15 -0400

    Update jcarpen3_vacuum.py
    significant changes to layout,
    
    go opposite direction based on where the bump is observed to be - attempting to suck dirt each time

--- jcarpen3_vacuum.py
+++ jcarpen3_vacuum.py
@@ -1,51 +1,32 @@
 from vacuum import VacuumAgent
 import random
 
-
 class Jcarpen3VacuumAgent(VacuumAgent):
 
     def __init__(self):
         super().__init__()
-        self.squares_visited = set()
-        self.current_square = (0, 0)
-        self.turn_off = False
+        self.direction = 'Right'
+
+
 
     def program(self, percept):
-        bumped, status = percept
+        bump, status = percept
 
-        if action_reach:
-            x, y = self.current_square
-            dx, dy = square[0] - x, square[1] - y
+        if status == 'Dirty':
+            return 'Suck'
 
-            if dx == 1:
-                return 'Right'
-            elif dx == -1:
-                return 'Left'
-            elif dy == 1:
-                return 'Up'
-            elif dy == -1:
-                return 'Down'
-
-        if get_adjacent_squares:
-            x, y = square
-            return [(x+1, y), (x-1, y), (x, y+1), (x, y-1)]
-
-        if choose_next_action:
-            next_square = self.get_adjacent_ squares(self.current_square)
-            for square in next_square:
-                if square not in self.square_visited:
-                    self.squares_visited.add(square)
-                    return self.action_reach(square)
-
-            
-
-            if status == 'Dirty':
-                self.turn_off = False
+        elif bump:
+            if self.direction == 'Right':
+                self.direction = 'Left'
                 return 'Suck'
-
-            if not self.turn_off:
-                next_action = self.choose_next_action()
-                if next_action:
-                    return next_action
-
-        return random.choice(self.possible_actions)
+            elif self.direction == 'Left':
+                self.direction = 'Right'
+                return 'Suck'
+            elif self.direction == 'Up':
+                self.direction = 'Down'
+                return 'Suck'
+            elif self.direction == 'Down':
+                self.direction = 'Up'
+                return 'Suck'
+        
+        return self.direction
