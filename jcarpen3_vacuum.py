c2c7fb8aa4e6190d0bdf5917a1db69961cfc3c72
jcarpenter23 <93732603+jcarpenter23@users.noreply.github.com>
2023-09-14 18:37:17 -0400

    Update jcarpen3_vacuum.py
    implemented get_next_square in attempt to keep track of where to go next
    
    changes to if bump occurs

--- jcarpen3_vacuum.py
+++ jcarpen3_vacuum.py
@@ -6,27 +6,42 @@ class Jcarpen3VacuumAgent(VacuumAgent):
     def __init__(self):
         super().__init__()
         self.direction = 'Right'
-
+        self.squares_cleaned = set()
 
 
     def program(self, percept):
         bump, status = percept
-
-        if status == 'Dirty':
-            return 'Suck'
-
-        elif bump:
-            if self.direction == 'Right':
-                self.direction = 'Left'
-                return 'Suck'
-            elif self.direction == 'Left':
-                self.direction = 'Right'
-                return 'Suck'
-            elif self.direction == 'Up':
-                self.direction = 'Down'
-                return 'Suck'
-            elif self.direction == 'Down':
-                self.direction = 'Up'
-                return 'Suck'
+        possible_directions = ['Up', 'Down', 'Right', 'Left']
         
+        
+        if status == 'Dirty':
+            self.squares_cleaned.add(self.location)
+            return 'Suck'
+        
+        if bump:
+            possible_directions = ['Up', 'Down', 'Right', 'Left']
+            possible_directions.remove(self.direction)
+            self.direction = random.choice(possible_directions)
+            return self.direction
+
+        
+        next_square = self.get_next_square()
+        if next_square in self.squares_cleaned:
+            return self.direction
+        else:
+            return 'Suck'
+            
         return self.direction
+
+    def get_next_square(self):
+        x, y = self.location
+        if self.direction == 'Up':
+            return (x, y + 1)
+        elif self.direction == 'Down':
+            return (x, y - 1)
+        elif self.direction == 'Right':
+            return (x + 1, y)
+        elif self.direction == 'Left':
+            return (x - 1, y)
+
+            
