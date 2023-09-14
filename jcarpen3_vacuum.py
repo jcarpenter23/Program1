0582c8f413d341f2b9256ad75665c928840db224
jcarpenter23 <93732603+jcarpenter23@users.noreply.github.com>
2023-09-14 13:53:14 -0400

    Update jcarpen3_vacuum.py
    attempt to implement a process that turns off the vacuum to save energy

--- jcarpen3_vacuum.py
+++ jcarpen3_vacuum.py
@@ -5,25 +5,33 @@ class Jcarpen3VacuumAgent(VacuumAgent):
 
     def __init__(self):
         super().__init__()
-        self.visited_squares = set()
+        self.squares_visited = set()
+        self.turn_off = False
 
     def program(self, percept):
         bump, status = percept
-        
-        current_status = self.location
-        
-    
+
+        current_square = (self.x, self.y)
+        self.visited_squares.add(current_square)
+
         if status == 'Dirty':
+            self.turn_off = False
             return 'Suck'
         
-        
         else:
-            possible_actions = [action for action in self.possible_actions if self.move_is_valid(action)]
-            undiscovered_squares = [action for action in possible_actions if self.get_location_from_action(action) not in self.square_visted]
+            if self.turn_off:
+                return 'Turnoff'
             
-            if undiscovered_squares:
-                next_action = random.choice(undiscovered_squares)
+            else:
+                possible_actions = [action for action in self.possible_actions if self.move_is_valid(action)]
+                unvisited_squares = [action for action in possible_actions if self.get_location_from_action(action) not in self.visited_squares]
+            
+            if unvisited_squares:
+                next_action = random.choice(unvisited_squares)
                 self.visited_squares.add(self.get_location_from_action(next_action))
                 return next_action
             else:
-                return random.choice(possible_actions)
+                self.turn_off = True
+                return 'NoOp'  # Stay in place if all squares are visited
+        
+       
