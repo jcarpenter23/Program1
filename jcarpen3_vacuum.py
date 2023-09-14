9cafd2331e16e10b33c5de5ba89b43ac277eb0e1
jcarpenter23 <93732603+jcarpenter23@users.noreply.github.com>
2023-09-14 13:07:25 -0400

    Update jcarpen3_vacuum.py
    initial updated to include set for visited squares
    changes made to if else statement regarding actions to take based on observation

--- jcarpen3_vacuum.py
+++ jcarpen3_vacuum.py
@@ -5,15 +5,25 @@ class Jcarpen3VacuumAgent(VacuumAgent):
 
     def __init__(self):
         super().__init__()
-        # any initialization you want to do here
+        self.visited_squares = set()
 
     def program(self, percept):
         bump, status = percept
-
+        
         current_status = self.location
-        self.swuare_achieved.add(current_status)
-
+        
+    
         if status == 'Dirty':
             return 'Suck'
+        
+        
         else:
-            return random.choice(self.possible_actions)
+            possible_actions = [action for action in self.possible_actions if self.move_is_valid(action)]
+            undiscovered_squares = [action for action in possible_actions if self.get_location_from_action(action) not in self.square_visted]
+            
+            if undiscovered_squares:
+                next_action = random.choice(undiscovered_squares)
+                self.visited_squares.add(self.get_location_from_action(next_action))
+                return next_action
+            else:
+                return random.choice(possible_actions)
